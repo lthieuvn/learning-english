83b018b83aee77e3eb43697ae637c8e92edd7b3e
hieule <hieutl@vietnews24.com>
2019-08-28 17:32:21 +0700

    Init structure, build webpack

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'WELCOME TO LEARN ENGLISH',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
